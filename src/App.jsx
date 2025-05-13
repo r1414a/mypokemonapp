77a6f3691989f2f262b51eec528a57c22527ae42
r1414a <rupeshchincholkar14@gmail.com>
2025-05-13 10:44:39 +0530

    added filter,sorting,localStorage favorites

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Layout from './Layout/Layout'
 import './App.css'
 import {Routes,Route} from 'react-router'
 import Home from "./pages/home/Home"
-import PokemonFavorites from './pages/Favorites/PokemonFavorites'
 
 function App() {
 
@@ -11,7 +10,6 @@ function App() {
     <Routes>
       <Route path='/' element={<Layout/>}>
         <Route index element={<Home/>}/>
-        <Route path='/favorites' element={<PokemonFavorites/>}/>
       </Route>
     </Routes>
   )
--- src/pages/favorites/PokemonFavorites.jsx
+++
@@ -1,8 +0,0 @@
-export default function PokemonFavorites() {
-    return (
-        <div>
-            <h1>Pokemon Favorites</h1>
-            <p>Here you can view your favorite Pokemon.</p>
-        </div>
-    )
-}
